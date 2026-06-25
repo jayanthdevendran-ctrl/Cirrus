77ef7617a9d8c55dd1cd84d321e98be620fa9954
jayanthdevendran-ctrl <jayanthdevendran@gmail.com>
2026-06-25 17:48:44 +0530

    attempted new stack approach

--- menustack.py
+++ menustack.py
@@ -8,27 +8,29 @@ from luma.core.render import canvas
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27)
 device = ili9341(serial, width=160, height=240)
 
-menu = {
-    "Music" : {"Liked songs" : likedsongsfunction,
-               "Playlists" : displayplaylists,
-               "Search" : opensearch,
-               "Local" : downloadedmusic},
+menus = { "Menu" : {
+    "Music" : {"Liked songs" : print("open liked songs"),
+               "Playlists" : print("opened playlists"),
+               "Search" : print("opened searc"),
+               "Local" : print("opened dl")},
 
-    "Games" : { "PS1" : {"Castlevania" : Launch castlevania,
-                        "Doom" : Launch Doom},
-                "Gameboy": Launchgameboy}
+    "Games" : { "PS1" : {"Castlevania" : print("Launch castlevania"),
+                        "Doom" : print("Launch Doom")},
+                "Gameboy": ("Launchgameboy")}}
         }        
-
-menustack = [menu]
+menustack = [menus]
 top = menustack(len) - 1
 topmenu = menustack[top]
-def drawmenu (topmenu, selected_index):
+def drawmenu (topmenu, selectedindex):
     with canvas(device) as draw:
-        for index, item in enumerate(topmenu):
-            if index == selected_index:
-                draw.text((10, 15*(index+1)), item, fill = "yellow")
-            else:
-                draw.text((10, 15*(index+1)), item, fill = "white")
+        indexes = topmenu.getkeys
+        global current_item
+        indexcurrent = indexes.index()
+        for sub in menus[topmenu]:
+            if sub == selectedindex:
+                draw.text((10, 15*()), item, fill = "yellow")
+
+
 
 down = Button(26)
 up = Button(24)
@@ -39,7 +41,7 @@ while True:
     uptrue = up.is_pressed
     selecttrue = select.is_pressed
     if downtrue == True:
-        if x<4:
+        if x<len():
             x = x+1
     if uptrue == True:
         if x>0:
@@ -51,4 +53,26 @@ while True:
 
     drawmenu(current_menu, x)
     time.sleep(0.2)
-    
+
+"""so we want a dictionary 
+main > MP3 > Playlists
+             liked songs
+             search
+    > Games > PS1 > MGS
+                    Granturismo 
+            > Doom
+            > Wolfenstein
+    > Settings > Volume
+                 brightness
+                 wifi
+                 etc
+
+so, we have a draw with canvas
+a dictionary each time, so i want to iterate through a dictionary                 
+            """
+for element in dict of current menu:
+    draw wtv is in the dictionary
+dict has to change for each menu
+dict = dict under each thing
+
+for ele in 
