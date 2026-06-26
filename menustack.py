6d5029c7d2a62e83ce8f9d7cd2f74d3339a40a72
jayanthdevendran-ctrl <jayanthdevendran@gmail.com>
2026-06-26 22:27:02 +0530

    menu stack

--- menustack.py
+++ menustack.py
@@ -8,71 +8,74 @@ from luma.core.render import canvas
 serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27)
 device = ili9341(serial, width=160, height=240)
 
-menus = { "Menu" : {
-    "Music" : {"Liked songs" : print("open liked songs"),
-               "Playlists" : print("opened playlists"),
-               "Search" : print("opened searc"),
-               "Local" : print("opened dl")},
+def openlikedsongs():
+    print
+def openplaylists():
+    print
+def opensearch():
+    print
+def openlocal():
+    print
+def opengames():
+    print
+def runDoom():
+    print
+def runPSGAME():
+    print
+def runGAMEBOY():
+    print
 
-    "Games" : { "PS1" : {"Castlevania" : print("Launch castlevania"),
-                        "Doom" : print("Launch Doom")},
-                "Gameboy": ("Launchgameboy")}}
-        }        
-menustack = [menus]
-top = menustack(len) - 1
-topmenu = menustack[top]
-def drawmenu (topmenu, selectedindex):
-    with canvas(device) as draw:
-        indexes = topmenu.getkeys
-        global current_item
-        indexcurrent = indexes.index()
-        for sub in menus[topmenu]:
-            if sub == selectedindex:
-                draw.text((10, 15*()), item, fill = "yellow")
+Doom = runDoom
+ps1 = runPSGAME
+Gameboy = runGAMEBOY
+LikedSongs = openlikedsongs
+Playlists = openplaylists
+Search = opensearch
+Local = openlocal
 
+Settings = ["Backlight", "Voume", "Go Back"]
+Games = [Doom, ps1, Gameboy, "Go Back"]
+Music = [LikedSongs, Playlists, Search, Local, "Go Back"]
+menus = [ Music, Games, Settings]
 
+stack = [menus] 
+currentmenu = stack[-1]
 
 down = Button(26)
 up = Button(24)
 select = Button(19)
 
+def draw_main(menu, selected_index):
+    with canvas(device) as draw:
+        for index, item in enumerate(menu):
+            if index == selected_index:
+                draw.text((10, 15*(index+1)), item, fill = "yellow")
+            else:
+                draw.text((10, 15*(index+1)), item, fill = "white")
+
+x = 0
 while True:
     downtrue = down.is_pressed
     uptrue = up.is_pressed
     selecttrue = select.is_pressed
-    if downtrue == True:
-        if x<len():
-            x = x+1
-    if uptrue == True:
-        if x>0:
-            x = x-1
-    if selecttrue == True:
-        menu.append{}
-        x = 0
-        drawmenu(current_menu, x)
+    if downtrue == True and x < len(currentmenu) - 1:
+        x = x+1
 
-    drawmenu(current_menu, x)
+    if uptrue == True and x > 0:
+        x = x-1
+
+    if selecttrue == True and currentmenu[x] == "Go Back":
+        x = 0
+        stack.pop()
+        
+    if selecttrue == True and currentmenu[x] != "Go Back":
+        if type(currentmenu[x]) == list:
+            stack.append(currentmenu[x])
+            x = 0
+        else:
+            currentmenu[x]()        
+
+    currentmenu = stack[-1]
+    draw_main(currentmenu, x)
     time.sleep(0.2)
 
-"""so we want a dictionary 
-main > MP3 > Playlists
-             liked songs
-             search
-    > Games > PS1 > MGS
-                    Granturismo 
-            > Doom
-            > Wolfenstein
-    > Settings > Volume
-                 brightness
-                 wifi
-                 etc
-
-so, we have a draw with canvas
-a dictionary each time, so i want to iterate through a dictionary                 
-            """
-for element in dict of current menu:
-    draw wtv is in the dictionary
-dict has to change for each menu
-dict = dict under each thing
-
-for ele in 
