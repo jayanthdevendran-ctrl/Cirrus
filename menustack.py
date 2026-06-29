8f84f1c509f83192bb226604a706d390817b00ff
jayanthdevendran-ctrl <jayanthdevendran@gmail.com>
2026-06-29 21:47:32 +0530

    final menu file

--- menustack.py
+++ menustack.py
@@ -28,73 +28,72 @@ def poweroff():
 def goback():
     x = 0
     stack.pop()
+def backlight():
+    print
+def volume():
+    print
 
-
-Doom = runDoom
-ps1 = runPSGAME
-Gameboy = runGAMEBOY
-LikedSongs = openlikedsongs
-Playlists = openplaylists
-Search = opensearch
-Local = openlocal
 
 menus = {"Menu" : {
          "Music": {"Liked Songs" : openlikedsongs, "Playlists" : openplaylists, "Search": opensearch, "Go Back": goback}, 
          "Games": {"Doom": runDoom, "PS1": runPSGAME, "Go Back": goback},
-         "Settings": ["Backlight", "Volume"] ,
+         "Settings": {"Backlight": backlight, "Volume": volume} ,
          "PowerOff": poweroff
         }}
 
-stack = ["Menu"] 
+stack = [menus["Menu"]] 
 currentmenu = stack[-1]
 
 down = Button(26)
 up = Button(24)
-select = Button(19)
+select = Button(19, bounce_time=0.05)
 
-def draw_main(menu, selected_index):
+def draw_menus(menu, selected_index):
+    keys = menu.keys()
     with canvas(device) as draw:
-        for index, item in enumerate(menu):
+        for index, item in enumerate(keys):
             if index == selected_index:
                 draw.text((10, 15*(index+1)), str(item), fill = "green")
             else:
                 draw.text((10, 15*(index+1)), str(item), fill = "gray")
 
-def draw_menus(menu, selected_index):
-    keys = menu.keys()
-    with canvas(device) as draw:
-        for item in menu:
-            if keys.index(item) == selected_index:
-                draw.text((10, 15*(selected_index+1)), str(item), fill = "green")
-            else:
-                draw.text((10, 15*(selected_index+1)), str(item), fill = "gray")
-
 x = 0
 while True:
+    keys = list(currentmenu.keys())
+    selected = currentmenu[keys[x]]
     downtrue = down.is_pressed
     uptrue = up.is_pressed
     selecttrue = select.is_pressed
+    
     if downtrue == True and x < len(currentmenu) - 1:
         x = x+1
+        selected = currentmenu[keys[x]]
 
     if uptrue == True and x > 0:
         x = x-1
-
-    if selecttrue == True and currentmenu[x] == "Go Back":
+        selected = currentmenu[keys[x]]
+    
+    if selecttrue == True and keys[x] == "Go Back":
         x = 0
+        print(stack)
+        print("WENTBACK")
         stack.pop()
+        time.sleep(1.5)
+        while select.is_pressed:
+            time.sleep(0.01)
         
-    if selecttrue == True and currentmenu[x] != "Go Back":
-        if type(currentmenu[x]) == list:
-            stack.append(currentmenu[x])
+    elif selecttrue == True and keys[x] != "Go Back":
+        print("ENTEREDMENU/RANFUNCTION")
+        if isinstance(selected, dict): 
+            stack.append(selected)
             x = 0
         else:
-            currentmenu[x]()
+            selected()
+            time.sleep(2)
+        while select.is_pressed:
+            time.sleep(0.01)
 
-    if selecttrue == True and currentmenu[x] == "Power Off":
-        poweroff()
-            
     currentmenu = stack[-1]
-    draw_main(currentmenu, x)
+    draw_menus(currentmenu, x)
     time.sleep(0.2)
 
